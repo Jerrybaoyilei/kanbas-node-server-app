75c288fbd53b7cff1870f56f299e1d0168305a76
Jerrybaoyilei <jerrybaoyilei@qq.com>
2023-12-04 20:48:10 -0500

    removed DB_CONNECTION_STRING from .env file. Locally code runs fine.

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ import cors from "cors";
 import mongoose from "mongoose";
 import UserRoutes from "./Users/routes.js";
 
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
+const CONNECTION_STRING = 'mongodb://127.0.0.1:27017/kanbas';
+console.log(process.env.DB_CONNECTION_STRING)
 mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(
